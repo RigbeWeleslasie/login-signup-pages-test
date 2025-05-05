89601671b3392ac7022e2b3c05b984f88d329789
Rigbe Weleslasie <rigbewweleslasie@gmail.com>
2025-05-05 22:01:22 +0300

    Test html login and signup pages

---
+++ signup.js
@@ -0,0 +1,9 @@
+const signup = (email, password) => {
+  if (!email || !password) {
+      throw new Error('Email and password are required.');
+  }
+  
+  return { success: true, message: 'Signup successful.' };
+};
+
+module.exports = {signup };
